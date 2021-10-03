1ad19cc5bc1d9c5114e5fc1f4df82c8df94b4449
pierreboehm <pierre.boehm@web.de>
2021-10-03 11:08:55 +0200

    adapt state-zoom-fragment for using renewed base-state-view

--- app/src/main/java/org/pb/android/ioiofish/fragment/StateZoomFragment.java
+++ app/src/main/java/org/pb/android/ioiofish/fragment/StateZoomFragment.java
@@ -1,14 +1,21 @@
 package org.pb.android.ioiofish.fragment;
 
+import android.annotation.SuppressLint;
+
 import androidx.fragment.app.Fragment;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.ViewById;
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 import org.pb.android.ioiofish.R;
+import org.pb.android.ioiofish.event.Events;
 import org.pb.android.ioiofish.ui.BaseStateView;
 
+@SuppressLint("NonConstantResourceId")
 @EFragment(R.layout.fragment_state_zoom)
 public class StateZoomFragment extends Fragment {
 
@@ -19,25 +26,47 @@ public class StateZoomFragment extends Fragment {
 
     @AfterViews
     public void initViews() {
-
+        baseStateView.setConnected();
+        //baseStateView.setConnecting();
+        EventBus.getDefault().postSticky(new Events.ServiceControlEvent(Events.ServiceControlEvent.ServiceState.START));
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        EventBus.getDefault().register(this);
     }
 
     @Override
     public void onPause() {
+        EventBus.getDefault().unregister(this);
         super.onPause();
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN, sticky = true)
+    public void onEvent(Events.PluggedStateChangedEvent event) {
+        EventBus.getDefault().removeStickyEvent(event);
+
+        if (event.isPlugged()) {
+            baseStateView.setConnected();
+        } else {
+            baseStateView.setDisconnected();
+        }
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onEvent(Events.RotationChangedEvent event) {
+        //baseStateView.runServos();
+        baseStateView.receiveGyroSignal(event.getPitch());
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onEvent(Events.SignalLevelReceivedEvent event) {
+        baseStateView.receiveSensorSignal();
     }
 
     @Click(R.id.baseStateView)
     public void onBaseStateViewClicked() {
-        if (baseStateView.isConnected) {
-            baseStateView.setDisconnected();
-        } else {
-            baseStateView.setConnected();
-        }
+        baseStateView.receiveSensorSignal();
     }
 }
