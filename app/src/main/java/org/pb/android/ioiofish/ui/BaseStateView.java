e0ffd6e83198db7096c9753fff615773aa45e3a6
pierreboehm <pierre.boehm@web.de>
2021-10-03 11:07:15 +0200

    adapt base-state-view for using animation-manager

--- app/src/main/java/org/pb/android/ioiofish/ui/BaseStateView.java
+++ app/src/main/java/org/pb/android/ioiofish/ui/BaseStateView.java
@@ -1,18 +1,21 @@
 package org.pb.android.ioiofish.ui;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.drawable.AnimationDrawable;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
-import androidx.appcompat.widget.AppCompatImageView;
-
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EViewGroup;
 import org.androidannotations.annotations.ViewById;
 import org.pb.android.ioiofish.R;
+import org.pb.android.ioiofish.animation.AnimationConfiguration;
+import org.pb.android.ioiofish.animation.AnimationManager;
 
+@SuppressLint("NonConstantResourceId")
 @EViewGroup(R.layout.view_base_state)
 public class BaseStateView extends LinearLayout {
 
@@ -20,12 +23,24 @@ public class BaseStateView extends LinearLayout {
     ImageView ivOval;
 
     @ViewById(R.id.ivLine)
-    AppCompatImageView ivLine;
+    ImageView ivLine;
+
+    @ViewById(R.id.ivLine2)
+    ImageView ivLine2;
 
     @ViewById(R.id.ivHexagon)
     ImageView ivHexagon;
 
+    @ViewById(R.id.ivStripesLeft)
+    ImageView ivStripesLeft;
+
+    @ViewById(R.id.ivStripesRight)
+    ImageView ivStripesRight;
+
     public boolean isConnected = false;
+
+    @Bean
+    AnimationManager animationManager;
 
     public BaseStateView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -33,24 +48,38 @@ public class BaseStateView extends LinearLayout {
 
     @AfterViews
     public void initView() {
-        ivLine.setBackgroundResource(R.drawable.connection_state_line_animation);
+
+    }
+
+    public void setConnecting() {
+        ivLine.setBackgroundResource(R.drawable.sensor_outgoing_signal_animation);
         AnimationDrawable frameAnimation = (AnimationDrawable) ivLine.getBackground();
         frameAnimation.start();
     }
 
     public void setConnected() {
-        isConnected = true;
-        ivOval.setBackgroundResource(R.drawable.iv_oval_teal_200_full);
-        ivLine.setBackgroundResource(R.drawable.iv_line_teal_200_full);
-        ivHexagon.setBackgroundResource(R.drawable.iv_hexagon_teal_200_full);
+        ivOval.setImageResource(R.drawable.iv_oval_teal_200_full);
+        ivLine.setImageResource(R.drawable.iv_line_dot_teal_200_full);
+        ivHexagon.setImageResource(R.drawable.iv_hexagon_teal_200_full);
+        ivLine2.setImageResource(R.drawable.iv_line_teal_200_full);
     }
 
     public void setDisconnected() {
-        isConnected = false;
-        ivOval.setBackgroundResource(R.drawable.iv_oval_teal_200_quarter);
-        ivLine.setBackgroundResource(R.drawable.connection_state_line_animation);
-        AnimationDrawable frameAnimation = (AnimationDrawable) ivLine.getBackground();
-        frameAnimation.start();
-        ivHexagon.setBackgroundResource(R.drawable.iv_hexagon_teal_200_quarter);
+        ivOval.setImageResource(R.drawable.iv_oval_teal_200_quarter);
+        ivLine.setImageResource(R.drawable.iv_line_dot_teal_200_quarter);
+        ivHexagon.setImageResource(R.drawable.iv_hexagon_teal_200_quarter);
+        ivLine2.setImageResource(R.drawable.iv_line_teal_200_quarter);
+    }
+
+    public void receiveSensorSignal() {
+        animationManager.startAnimation(ivLine, AnimationConfiguration.SENSOR_DATA_INCOMING);
+    }
+
+    public void receiveGyroSignal(float pitchLevel) {
+        if (pitchLevel >= 0f) {
+            animationManager.startAnimation(ivLine2, AnimationConfiguration.GYRO_DATA_INCOMING_RIGHT);
+        } else {
+            animationManager.startAnimation(ivLine2, AnimationConfiguration.GYRO_DATA_INCOMING_LEFT);
+        }
     }
 }
